4787e1b149d533ed5c9810ce5f4bd8578c5d8037
qyyqstilllearning <capt.rizqy@gmail.com>
2025-09-03 21:09:31 +0700

    first commit

---
+++ resources/views/catalog/index.blade.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catalog</title>
+</head>
+<body>
+    <h2>Abundant become simple.</h2>
+
+    <ul>
+        <li>Code 1.sh</li>
+        <li>Code 2.py</li>
+        <li>Code 3.c</li>
+    </ul>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MySimple</title>
+</head>
+<body>
+    <h1>Welcome!</h1>
+    <p>MySimple to keep your code in simplicity</p>
+
+    <a href="/catalog" class="">
+        keep it simple.
+    </a>
+</body>
+</html>
